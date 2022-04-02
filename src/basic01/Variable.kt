28721964202d5b468ea5255f8e85c610b0ec81e6
PCY <pcy4196@naver.com>
2022-04-02 16:27:18 +0900

    2022.04.02 -- commit 01.변수와 자료형 basic01.Variable

--- src/basic01/Variable.kt
+++ src/basic01/Variable.kt
@@ -4,7 +4,7 @@ class Variable {
 }
 
 fun main() {
-    // 정수 리터럴
+    // 1. 정수 리터럴
     println(100);
     println(1000000000000000000);
     println(1000000000000000000L);
@@ -70,5 +70,80 @@ fun main() {
     var7 = "동해물과 백두산이\n마르고 닳도록\n하느님이 보우하사\nn우리나라 만세";
     var1 = false;
     System.out.println(var7);
+     */
+
+    println("-------------------------------------------------");
+
+    // 2.변수
+    val a1:Int = 100;
+    println("a1 : ${a1}");
+    println("a1 : $a1");    // 변수만 {} 생략가능
+
+    // 변수 선언 자료형을 생략하면 저장하는 값에 따라 자동으로 결정된다.
+    val a2 = 100;           // 자료형 생략 가능
+    println("a2 : ${a2}");
+    /*
+    --JAVA 변환
+    a1 = 'd'
+    int a2 = 100;
+     */
+
+    var a3:Int = 100;
+    val a4:Int = 100;
+
+    println("a3 : ${a3}");
+    println("a4 : ${a4}");
+
+    a3 = 200;
+//    a4 = 200; // 오류 발생 -- val 변수는 값을 다시 저장하지 못한다.
+    println("a3 : ${a3}");
+    /*
+    -- JAVA 변환
+    int a3 = 100;
+    int a4 = 100;
+    String var4 = "a3 : " + a3;
+    System.out.println(var4);
+    var4 = "a4 : " + a4;
+    System.out.println(var4);
+    int a3 = 200;
+    var4 = "a3 : " + a3;
+    System.out.println(var4);
+     */
+
+    println("-------------------------------------------------");
+
+    // 3. null 허용 변수
+    var a5:Int = 100; // null 선언 불가
+    var a6:Int? = null;
+
+    println("a5 : ${a5}");
+    println("a6 : ${a6}");
+
+    var a7:Int? = a5;
+    println("a7 : ${a7}");
+
+//    var a8:Int = a6; // 오류 발생
+    var a8:Int = a6!!; // !! --> null 허용하지 않는 변수로 변환
+    println("a8 : ${a8}");
+    /*
+    Exception in thread "main" java.lang.NullPointerException
+        at basic01.VariableKt.main(Variable.kt:126)
+        at basic01.VariableKt.main(Variable.kt)
+     */
+    /*
+    -- JAVA 변환
+    int a5 = 100;
+    Integer a6 = (Integer)null;
+    String var6 = "a5 : " + a5;
+    System.out.println(var6);
+    var6 = "a6 : " + a6;
+    System.out.println(var6);
+    Integer a7 = Integer.valueOf(a5);
+    String var25 = "a7 : " + a7;
+    System.out.println(var25);
+    Intrinsics.checkNotNull(a6);
+    int a8 = a6;
+    String var27 = "a8 : " + a8;
+    System.out.println(var27);
      */
 }
